8fddd1bfcc799858b4b92c2bb39357f34f54a497
Keeeeecz <2573701311@qq.com>
2018-05-31 12:56:01 +0800

    video

--- NaviTest/app/src/main/java/com/example/administrator/navitest/MainActivity.java
+++ NaviTest/app/src/main/java/com/example/administrator/navitest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.administrator.navitest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,7 +15,9 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -21,9 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //安卓硬件相机
     private Camera mCamera;
     private SurfaceHolder mHolder;
-    public static final String FILE_PATH = "filePath";
-    private int mPictureCount=9;//默认自动拍三张
-    int pic_count=0;
+    private int mPictureCount=9;//连拍次数
+    int pic_count=0;//记录当前已拍张数
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,15 +88,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void startTakephoto() {
         //获取到相机参数
         Camera.Parameters parameters = mCamera.getParameters();
-        //设置图片保存格式
-        parameters.setPictureFormat( ImageFormat.JPEG);
+        parameters.setPreviewFpsRange(10, 20); // 每秒显示20~30帧
+        parameters.setPictureFormat(ImageFormat.NV21); // 设置图片格式
         //设置图片大小
         parameters.setPreviewSize(480, 720);
         //设置对焦
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
         //设置自动对焦
         Log.e("Count",Integer.toString( pic_count ));
-        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+        //拍照
+        /*mCamera.autoFocus(new Camera.AutoFocusCallback() {
             @Override
             public void onAutoFocus(boolean success, Camera camera) {
                 if (success) {
@@ -107,10 +113,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     });
                 }
             }
+        });*/
+
+        //录像
+        mCamera.setPreviewCallback(new Camera.PreviewCallback(){
+            @Override
+            public void onPreviewFrame(byte[] data, Camera camera) {
+                Camera.Size size = camera.getParameters().getPreviewSize();
+                try{
+                    YuvImage image = new YuvImage(data, ImageFormat.NV21, size.width, size.height, null);
+                    if(image!=null){
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        image.compressToJpeg(new Rect(0, 0, size.width, size.height), 80, stream);
+                        Bitmap bmp = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
+                        //因为图片会放生旋转，因此要对图片进行旋转到和手机在一个方向上
+                        rotateMyBitmap(bmp);
+                        saveBitmap( bmp );
+                        stream.close();
+                    }
+                }catch(Exception ex){
+                    Log.e("Sys","Error:"+ex.getMessage());
+                }
+            }
         });
     }
+    public void rotateMyBitmap(Bitmap bmp){
+        //*****旋转一下
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap nbmp2 = Bitmap.createBitmap(bmp, 0,0, bmp.getWidth(),  bmp.getHeight(), matrix, true);
+    };
+    public void saveBitmap(Bitmap bm) {
+        //保存录像图片
+        Log.e("TAG", "保存图片");
+        String fileName = "/sdcard/1/" + System.currentTimeMillis() + ".jpg"; //保存路径
+        File f = new File( fileName );
+        if (f.exists()) {
+            f.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.JPEG, 90, out);//bitmap的压缩方法
+            out.flush();
+            out.close();
+            Log.i("TAG", "已经保存");
+        } catch (FileNotFoundException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-    //保存拍照数据
+/*    //保存拍照数据
    class dealWithCameraData extends AsyncTask<byte[], String, String> {
         @Override
         protected void onPreExecute() {
@@ -133,7 +190,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             return null;
         }
-    }
+    }*/
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
